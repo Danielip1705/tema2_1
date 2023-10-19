8dc6d41d3473629c2d6f99ce234601bc4cba46ec
Danielip1705 <daniel.iglesias@iesnervion.es>
2023-10-19 13:50:02 +0200

    do-while

--- src/ejercicio_dowhile/Ejercicio6.java
+++ src/ejercicio_dowhile/Ejercicio6.java
@@ -11,7 +11,7 @@ public class Ejercicio6 {
 		String papel="papel";
 		String player1;
 		String player2;
-		
+		String rendicion="S";
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Jugador1, eliga piedra,papel o tijera");
@@ -24,8 +24,14 @@ public class Ejercicio6 {
 			if (player1==player2) {
 				System.out.println("EMPATE");
 			}else if (player1.equalsIgnoreCase(papel)&&player2.equalsIgnoreCase(piedra)){
+			} else if(player1.equalsIgnoreCase(piedra)&&player2.equalsIgnoreCase(tijera)) {
+			} else if (player1.equalsIgnoreCase(tijera)&&player2.equalsIgnoreCase(papel)) {
+				System.out.println("Gana jugador1");
+			} else {
+				System.out.println("Gana jugador2");
+			}
 				
-		} while ();
+		} while (player1==rendicion&&player2==rendicion);
 		
 		
 		
